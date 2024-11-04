1b739fae7ca8a669f129b9a6b65e816d1582a123
OLogN <vinhhiepbn2003.work@gmail.com>
2024-11-05 01:54:10 +0700

    Updated

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -84,7 +84,6 @@ def tracking():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    options.add_argument("--remote-debugging-port=9222")
     
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
