761e853466412764802644488b9857af7a805cae
OLogN <vinhhiepbn2003.work@gmail.com>
2024-11-05 16:28:07 +0700

    Updated

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -93,7 +93,6 @@ def tracking():
     # WebDriverWait(driver, 7200).until(EC.presence_of_all_elements_located(
     #     (By.XPATH, "/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button")))
 
-    print("Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
     driver.find_element(By.NAME, "email").send_keys(username, Keys.ESCAPE)
 
     driver.find_element(By.NAME, "pass").send_keys(password, Keys.ENTER)
@@ -101,7 +100,9 @@ def tracking():
     WebDriverWait(driver, 7200).until(EC.presence_of_all_elements_located(
         (By.CLASS_NAME, "xe3v8dz")))        # wait for the presence of main page
 
-    driver.get(info[2])     # Access the main page
+    # driver.get(info[2])     # Access the main page
+
+    driver.get("https://www.facebook.com/doanchinhit2102")
 
     WebDriverWait(driver, 7200).until(EC.presence_of_all_elements_located(
         (By.CLASS_NAME, "x1n2onr6.x1ja2u2z.x78zum5.x2lah0s.xl56j7k.x6s0dn4.xozqiw3.x1q0g3np.xi112ho.x17zwfj4.x585lrc.x1403ito.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.xn6708d.x1ye3gou.xtvsq51.x1r1pt67")))        # wait for the presence of "Nhắn tin" button
